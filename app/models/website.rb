a785bda1fd28b45b40689450f68ffb56bd37cce0
DennisDaddy <softteez95@gmail.com>
2017-04-30 12:10:58 +0300

    try removing error

--- app/models/website.rb
+++ app/models/website.rb
@@ -1,6 +1,23 @@
+require 'open-uri'
+
 class Website < ApplicationRecord
+
 	def headline
-		"Hey it's a headline"
-		
+
+		begin
+
+		file = open("http://#{name}")
+
+		contents = file.read
+		unless contents.index("<h1>").nil?
+			header_start = contents.index("<h1>") + 4
+			header_end = contents.index("</h1>") - 1
+			p contents[header_start..header_end]
+		else
+			p "There were no h1 tags on #{name}."			
+		end
+	rescue
+		puts "You typed in something wrong. please try again"
+	end
 	end
 end
